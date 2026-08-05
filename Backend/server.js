94aca276da5ffb817fc0870fdc3a71fb25ecb372
NEO MONYEBODI <neo1monyebodi@gmail.com>
2026-08-05 17:34:47 +0200

    added official domains/https on server.js

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,10 @@ app.use(cors({
     origin: ['http://localhost:3001',
              'http://127.0.0.1:3001',
              'https://bakari-health-frontend.onrender.com',
-             'https://bakari-health.onrender.com'],
+             'https://bakari-health.onrender.com',
+             'https://www.bakari-health.co.za',
+             'https://bakari-health.co.za'
+            ],
     credentials: true,
 }));
 app.use(express.json());
